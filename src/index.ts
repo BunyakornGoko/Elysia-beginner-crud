25463594feeed2f39381aabce50dd40efe596b3c
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2025-03-07 19:27:18 +0700

    Add authorization

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ import { user } from './user'
 
 const app = new Elysia()
     .use(swagger())
+    .onError(({ error, code }) => { 
+      if (code === 'NOT_FOUND') return 'Not Found :('
+
+      console.error(error) 
+    }) 
     .use(user)
     .use(note) 
     .listen(3000)
--- src/note.ts
+++ src/note.ts
@@ -1,67 +1,91 @@
 import { Elysia, t } from 'elysia'
+import { getUserId, userService } from './user'
+
+const memo = t.Object({
+	data: t.String(),
+	author: t.String()
+})
+
+type Memo = typeof memo.static
 
 class Note {
-    constructor(public data: string[] = ['Moonhalo']) {}
+    constructor(
+		public data: Memo[] = [
+			{
+				data: 'Moonhalo',
+				author: 'saltyaom'
+			}
+		]
+	) {}
 
-    add(note: string) { 
-        this.data.push(note) 
+    add(note: Memo) {
+        this.data.push(note)
 
-        return this.data 
-    } 
+        return this.data
+    }
 
-    remove(index: number) { 
-        return this.data.splice(index, 1) 
-    } 
+    remove(index: number) {
+        return this.data.splice(index, 1)
+    }
 
-    update(index: number, note: string) { 
-        return (this.data[index] = note) 
-    } 
-
+    update(index: number, note: Partial<Memo>) {
+        return (this.data[index] = { ...this.data[index], ...note })
+    }
 }
 
-export const note = new Elysia({ prefix: '/note'})
+export const note = new Elysia({ prefix: '/note' })
+	.use(userService) 
     .decorate('note', new Note())
-    .onTransform(function log({ body, params, path, request: { method } }) { 
-        console.log(`${method} ${path}`, { 
-            body, 
-            params 
-        }) 
-    }) 
+    .model({
+        memo: t.Omit(memo, ['author'])
+    })
+    .onTransform(function log({ body, params, path, request: { method } }) {
+        console.log(`${method} ${path}`, {
+            body,
+            params
+        })
+    })
     .get('/', ({ note }) => note.data)
-    .put('/note', ({ note, body: { data } }) => note.add(data), { 
-        body: t.Object({ 
-            data: t.String() 
-        }) 
-    }) 
-    .guard({ 
-        params: t.Object({ 
-            index: t.Number() 
-    }) 
-    }) 
-    .get(
-        '/note/:index',
-        ({ note, params: { index }, error }) => {
-            return note.data[index] ?? error(404, 'oh no :(')
+    .use(getUserId) 
+    .put(
+        '/',
+        ({ note, body: { data }, username }) =>
+            note.add({ data, author: username }),
+        {
+            body: 'memo'
         }
     )
-    .delete( 
-        '/note/:index', 
-        ({ note, params: { index }, error }) => { 
-            if (index in note.data) return note.remove(index) 
-
-            return error(422) 
+    .get(
+        '/:index',
+        ({ note, params: { index }, error }) => {
+            return note.data[index] ?? error(404, 'Not Found :(')
+        },
+        {
+            params: t.Object({
+                index: t.Number()
+            })
         }
-    ) 
-    .patch( 
-        '/note/:index', 
-        ({ note, params: { index }, body: { data }, error }) => { 
-            if (index in note.data) return note.update(index, data) 
+    )
+    .guard({
+        params: t.Object({
+            index: t.Number()
+        })
+    })
+    .delete('/:index', ({ note, params: { index }, error }) => {
+        if (index in note.data) return note.remove(index)
 
-            return error(422) 
-        }, 
-        { 
-            body: t.Object({ 
-                data: t.String() 
-            }) 
-        } 
-    ) 
+        return error(422)
+    })
+    .patch(
+        '/:index',
+        ({ note, params: { index }, body: { data }, error, username }) => {
+            if (index in note.data)
+                return note.update(index, { data, author: username })
+
+            return error(422)
+        },
+        {
+            isSignIn: true,
+            body: 'memo'
+        }
+    )
